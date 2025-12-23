ebdcb9c84cdb6628c89183b15e9745d5fee28c97
Re:J0 <wogus9728@gmail.com>
2025-12-23 19:10:53 +0900

    섹션1:5
    1. 유니티 qwert 소개
    2. 자그마한 2d 그림 그리기
    3. C# 스크립트 추가
    4. 스크립트 start() 함수 의미 확인
    5. start 함수에 로그 출력 테스트

---
+++ Assets/PrintToConsole.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PrintToConsole : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("도넛을 너무 먹었나 밧줄이 끊어질것같아");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
